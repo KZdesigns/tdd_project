25533e684ef4269dfd3a3e8f3d13082297710fb6
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-06-19 11:59:35 -0700

    game complete and passing all tests

--- lib/towers_of_hanoi.rb
+++ lib/towers_of_hanoi.rb
@@ -6,14 +6,59 @@ class TowersOfHanoi
     end
 
     def valid_move?(from_tower, to_tower)
-        return false if from_tower.empty?
+        return false unless [from_tower, to_tower].all? { |i| i.between?(0, 2) }
 
         !@stacks[from_tower].empty? && (
-            @stacks[from_tower].last < @stacks[to_tower].last ||
-            @stacks[to_tower].empty?
+            @stacks[to_tower].empty? ||
+            @stacks[from_tower].last < @stacks[to_tower].last 
         )
     end 
 
-    
+    def move(from_tower, to_tower)
+        raise "Cannot move from an empty tower." if @stacks[from_tower].empty?
+        raise "Invalid move." unless valid_move?(from_tower, to_tower)
+        @stacks[to_tower] << @stacks[from_tower].pop
+    end
 
+    def won?
+        @stacks[0].empty? && @stacks[1..2].any?(&:empty?)
+    end
+
+    def render
+        'Tower 0: ' + @stacks[0].join(' ') + "\n" +
+        'Tower 1: ' + @stacks[1].join(' ') + "\n" +
+        'Tower 2: ' + @stacks[2].join(' ') + "\n"
+    end
+
+    def display
+        system('clear')
+        puts render
+    end
+
+    def play
+        display
+
+        until won?
+            puts 'Which tower would you like to take from?'
+            from_tower = gets.to_i
+
+            puts 'Which tower would you like to move to?'
+            to_tower = gets.to_i
+
+            if valid_move?(from_tower, to_tower)
+                move(from_tower, to_tower)
+                display
+            else
+                display
+                puts "Invalid move please try again."
+            end
+        end
+
+        puts "You win!"
+    end
+end
+
+
+if $PROGRAM_NAME == __FILE__
+    TowersOfHanoi.new.play
 end
--- spec/towers_of_hanoi_spec.rb
+++ spec/towers_of_hanoi_spec.rb
@@ -1,23 +1,72 @@
 require 'towers_of_hanoi'
 
-describe 'TowersOfHanoi' do 
-    let(:game) { TowersOfHanoi.new }
-    
-    describe '#initialize' do
-        it 'start assign the stacks instance variable' do 
-            expect(game.stacks).to eq([[3,2,1],[],[]])
-        end
+describe TowersOfHanoi do
+  subject(:towers) { TowersOfHanoi.new }
+
+  describe "#render" do
+    it "pretty-prints stacks" do
+      expect(towers.render).to eq("Tower 0: 3 2 1\nTower 1: \nTower 2: \n")
     end
 
+    it "prints shorter stacks" do
+      towers.move(0, 2)
+      towers.move(0, 1)
+      expect(towers.render).to eq("Tower 0: 3\nTower 1: 2\nTower 2: 1\n")
+    end
+  end
 
+  describe "#move" do
+    it "allows moving to a blank space" do
+      expect { towers.move(0, 1) }.not_to raise_error
+    end
+    
+    it "allows moving onto a larger disk" do
+      towers.move(0, 2)
+      towers.move(0, 1)
+      expect { towers.move(2, 1) }.not_to raise_error
+    end
 
+    it "does not allow moving from an empty stack" do
+      expect { towers.move(1, 2) }
+      .to raise_error("Cannot move from an empty tower.")
+    end
 
+    it "does not allow moving onto a smaller disk" do
+      towers.move(0, 1)
+      expect { towers.move(0, 1) }
+      .to raise_error("Invalid move.")
+    end
+  end
 
+  describe "#won?" do
+    it "is not won at the start" do
+      expect(towers).not_to be_won
+    end
 
+    it "is won when all disks are moved to tower 1" do
+      # Perform the moves to move the game into winning position
+      towers.move(0, 1)
+      towers.move(0, 2)
+      towers.move(1, 2)
+      towers.move(0, 1)
+      towers.move(2, 0)
+      towers.move(2, 1)
+      towers.move(0, 1)
 
+      expect(towers).to be_won
+    end
 
+    it "is won when all disks are moved to tower 2" do
+      # Perform the moves to move the game into winning position
+      towers.move(0, 2)
+      towers.move(0, 1)
+      towers.move(2, 1)
+      towers.move(0, 2)
+      towers.move(1, 0)
+      towers.move(1, 2)
+      towers.move(0, 2)
 
-
-
-
+      expect(towers).to be_won
+    end
+  end
 end
